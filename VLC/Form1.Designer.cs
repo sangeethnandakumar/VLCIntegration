a4ca166c5ee1ba1fa991a36d3e7a730a06c18bec
Sangeeth Nandakumar <twileloop@outlook.com>
2022-06-15 00:29:03 +0530

    Add project files.

---
+++ VLC/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace VLC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.VLCPlayer = new Vlc.DotNet.Forms.VlcControl();
+            ((System.ComponentModel.ISupportInitialize)(this.VLCPlayer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // VLCPlayer
+            // 
+            this.VLCPlayer.BackColor = System.Drawing.Color.Black;
+            this.VLCPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.VLCPlayer.Location = new System.Drawing.Point(0, 0);
+            this.VLCPlayer.Name = "VLCPlayer";
+            this.VLCPlayer.Size = new System.Drawing.Size(737, 470);
+            this.VLCPlayer.Spu = -1;
+            this.VLCPlayer.TabIndex = 0;
+            this.VLCPlayer.Text = "vlcControl1";
+            this.VLCPlayer.VlcLibDirectory = ((System.IO.DirectoryInfo)(resources.GetObject("VLCPlayer.VlcLibDirectory")));
+            this.VLCPlayer.VlcMediaplayerOptions = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(737, 470);
+            this.Controls.Add(this.VLCPlayer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.VLCPlayer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Vlc.DotNet.Forms.VlcControl VLCPlayer;
+    }
+}
+
---
+++ VLC/Form1.cs
@@ -0,0 +1,51 @@
+﻿using DirectShowLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VLC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            //Get capture devices
+            var devices = new List<DsDevice>(DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice));
+            var cameraNames = new List<string>();
+            foreach (var device in devices)
+            {
+                cameraNames.Add(device.Name);
+            }
+
+            //Use the same VLC options
+            var mediaOptions = new[]
+                 {
+                    ":dshow-vdev=HD User Facing",
+                    ":dshow-adev=Microphone Array (Realtek(R) Audio)",
+                    ":live-caching=500",
+                    ":sout-keep"
+                };
+
+            VLCPlayer.SetMedia("dshow://", mediaOptions);
+
+            VLCPlayer.Play();
+
+            Console.WriteLine("Streaming on rtsp://127.0.0.1:554/");
+            Console.WriteLine("Press any key to exit");
+
+            //VLCPlayer.Play(new Uri("http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"));
+        }
+    }
+}
